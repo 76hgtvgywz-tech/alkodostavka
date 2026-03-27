846b4ebc0dabec724a2dd249ecc6781c23af37f2
76hgtvgywz-tech <76hgtvgywz@privaterelay.appleid.com>
2026-03-27 05:14:59 +0400

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
-// Telegram bot credentials (замените на свои)
-const TELEGRAM_TOKEN = '8577126925:AAFKxUZuaUZIs-b3yJuA_ZiWJHTp27tfLs8';
+// config.js
+const TELEGRAM_TOKEN = '8427344243:AAFiXfheHb9HmRa2K5MwJR4o7fjXzGRIPa4';
 const CHAT_ID = '562345561';
